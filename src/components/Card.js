9e0c7a28150eae2537928999632fd6d42c807fe1
KuilDi <kuildi@yandex.ru>
2020-04-09 14:58:47 +0400

    Редактирование Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,9 +11,6 @@ export default class Card extends React.Component {
                 <div className="card__content text-overflow_fade">
                     <p>{this.props.text}</p>
                 </div>
-                <footer className="card__footer">
-                    <Button textBtn="See more" class="btn_primary-color" />
-                </footer>
             </aside>
         );
     }
