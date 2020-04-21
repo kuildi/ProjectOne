a0ab9bbd23e092ca14247b1a84f8e5594b714420
KuilDi <kuildi@yandex.ru>
2020-04-21 19:21:05 +0400

    Удален srcset

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,6 @@ export default class Navbar extends React.Component {
             { id: "main", href: "/", title: "Главная" },
             { id: "football", href: "/football", title: "Футбол" },
             { id: "users", href: "/users", title: "Пользователи" },
-            // { id: "user", href: "/user", title: "Пользователь" },
             { id: "contacts", href: "/contacts", title: "Контакты" },
             { id: "gallery", href: "/gallery", title: "Галлерея" }
         ];
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Photo extends React.Component {
 	render() {
 		return (
-			<img src={this.props.url} alt={this.props.title} srcset=""/>
+			<img src={this.props.url} alt={this.props.title} />
 		);
 	}
 }
