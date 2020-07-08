1c052d54fb5875e6c8123201565eeec9371f04b9
KuilDi <kuildi@yandex.ru>
2020-07-08 16:12:48 +0400

    Сетка страницы "обо мне"

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -16,30 +16,30 @@ export default function About() {
     return (
         <article className="content-wrap section_space-v_l">
             <section className="grid-container grid-container_about grid-container_indent-b_l">
-                <div className="grid-container__section_xl grid-container__sub-grid">
-                    <div className="tile-about tile-about_flex">
-                        <p className="primary-text">На текущем месте отвечаю за верстку и переход проекта на новый дизайн. Координирую работу двух коллег при работе с версткой. Делаю кодревью всех задач, затрагивающих верстку и стили проекта.
-                        Занимаюсь рефакторингом стилей и внедрением семантики HTML5.
+                <div className="tile-about tile-about_flex grid-container__section-work">
+                    <p className="primary-text">На текущем месте отвечаю за верстку и переход проекта на новый дизайн. Координирую работу двух коллег при работе с версткой. Делаю кодревью всех задач, затрагивающих верстку и стили проекта.
+                    Занимаюсь рефакторингом стилей и внедрением семантики HTML5.
                         </p>
-                        <br />
-                        <p className="primary-text">Так же в своей работе использую JavaScript для создания различных интерактивностей.</p>
-                    </div>
-                    <section>
-                        <img className="photo photo_rounded" src={image} alt="Its me" />
-                        <img className="photo photo_rounded" src={image} alt="Its me" />
-                        <img className="photo photo_rounded" src={image} alt="Its me" />
-                    </section>
+                    <br />
+                    <p className="primary-text">Так же в своей работе использую JavaScript для создания различных интерактивностей.</p>
                 </div>
-                <div className="grid-container__section_xxl grid-container__sub-grid">
-                    <div className="tile-about">
-                        <p className="primary-text">
-                            Окончил ВолгГТУ, факультет электроники и вычеслительной техники по направлению “Системы автоматизированного проектирования и поискового конструирования”.
-                            Дипломный проект "Разработка интерактивного робота и веб-приложения для его управления".
+                <div className="tile-about grid-container__section-education">
+                    <p className="primary-text">
+                        Окончил ВолгГТУ, факультет электроники и вычеслительной техники по направлению “Системы автоматизированного проектирования и поискового конструирования”.
+                        Дипломный проект "Разработка интерактивного робота и веб-приложения для его управления".
                         </p>
-                    </div>
-                    <div className="tile-about">
-                        <p className="primary-text">Люблю футбол, музыку, вкусную еду, свою девушку и кота. Интересуюсь веб-дизайном, фильмами и игровой индустрией. Занимался английским в онлайн школе SkyEng, улучшил свой уровень до Intermediate.</p>
-                    </div>
+                </div>
+                <div className="grid-container__section-photo_first">
+                    <img className="photo photo_rounded" src={image} alt="Its me" />
+                </div>
+                <div className="grid-container__section-photo_second">
+                    <img className="photo photo_rounded" src={image} alt="Its me" />
+                </div>
+                <div className="grid-container__section-photo_third">
+                    <img className="photo photo_rounded" src={image} alt="Its me" />
+                </div>
+                <div className="tile-about grid-container__section-hobby">
+                    <p className="primary-text">Люблю футбол, музыку, вкусную еду, свою девушку и кота. Интересуюсь веб-дизайном, фильмами и игровой индустрией. Занимался английским в онлайн школе SkyEng, улучшил свой уровень до Intermediate.</p>
                 </div>
             </section>
             <a href="mailto:kuildi@yandex.ru">
