69c81a0f1c6b1142c7086fa3ed421d05945f3354
KuilDi <kuildi@yandex.ru>
2020-07-31 16:41:31 +0400

    Правка текста на главной странице

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -7,7 +7,7 @@ const Main = () => (
         <h1><span className="gradient-color">Здравствуйте,</span> я Кузнецов Илья - </h1>
         <h2 className="primary-text_indent-b_l">
             Фронтенд разработчик из Волгограда.
-            В настоящее время работаю над версткой в компании <a className="text-link" href="https://www.danfoss.com/ru-ru/">Danfoss</a> .
+            В настоящее время работаю над версткой одного из проектов компании <a className="text-link" href="https://www.danfoss.com/ru-ru/">Danfoss</a>
             </h2>
         <Link to={`/skills`}>
             <Button icon="chevron-right">Мои навыки</Button>
