4b13ee44cf51fed278aa549e4e4bb3aa7862c108
Ilya Kuznetsov <30955203+kuildi@users.noreply.github.com>
2022-08-16 13:19:19 +0300

    update main page text

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -7,7 +7,7 @@ const Main = () => (
         <h1><span className="gradient-color">Здравствуйте,</span> я Кузнецов Илья - </h1>
         <h2 className="text_indent-b_l">
             Frontend-разработчик из Волгограда.
-            В настоящее время работаю над одним из проектов компании <a className="text_link" href="https://www.danfoss.com/ru-ru/">Danfoss</a>
+            В настоящее время в поисках новой компании
             </h2>
         <Link to={`/skills`}>
             <Button icon="chevron-right">Мои навыки</Button>
